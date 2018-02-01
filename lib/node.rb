607cc492b4239e5050dbb773ef8f73186047a7d9
JNelson <nelson.jimmy@gmail.com>
2018-02-01 05:42:14 -0700

    I'm not sure what the changes were to this file. Probably not large...

--- lib/node.rb
+++ lib/node.rb
@@ -1,15 +1,41 @@
 require 'pry'
 
 class Node
-  attr_reader :score, :movie_title
+  attr_reader :value, :movie_title
   attr_accessor :left, :right, :depth
 
-  def initialize(score, movie_title)
-    @score = score
+  def initialize(value, movie_title, depth=0)
+    @value = value
     @movie_title = movie_title
-    @left = nil;
-    @right = nil;
-    @depth = 0
+    @left = nil
+    @right = nil
+    @depth = depth
   end
 
+  # def move_left
+  #   if @root_node.left.nil?
+  #     @root_node.left = @current_node
+  #     @current_node.depth
+  #   else
+  #     @root_node.left.insert(score, movie_title)
+  #   end
+  # end
+  #
+  # def move_right
+  #   if @root_node.right.nil?
+  #     @root_node.right = @current_node
+  #     @current_node.depth
+  #   else
+  #     @root_node.right.insert(score, movie_title)
+  #   end
+  # end
+
+  # def insert(new_score, new_movie, depth=0)
+  #   if new_score <= @score
+  #     @left.nil? ? @left = Node.new(new_score, new_movie) : @left.insert(new_score, new_movie)
+  #   elsif new_score > @score
+  #     @right.nil? ? @right = Node.new(new_score, new_movie) : @right.insert(new_score, new_movie)
+  #   end
+  #
+  # end
 end
